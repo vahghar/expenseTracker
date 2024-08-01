6db9c53a0483f6c7c611f1af914cc37368a11598
G-Samarth <g.samarth@outlook.com>
2024-08-02 02:21:42 +0530

    frontend-done

---
+++ frontend/src/components/Auth/AuthRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+import { getUserFromStorage } from "../../utils/getUserFromStorage";
+
+const AuthRoute = ({ children }) => {
+  //get the token
+  const token = getUserFromStorage();
+
+  if (token) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
+
+export default AuthRoute;
---
+++ frontend/src/utils/getUserFromStorage.js
@@ -0,0 +1,6 @@
+//this is to get the token
+
+export const getUserFromStorage = () => {
+    const token = JSON.parse(localStorage.getItem("userInfo") || null);
+    return token?.token;
+  };
